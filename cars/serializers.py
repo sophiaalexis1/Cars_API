95ef8f94092d9ea87f3bd29da2c0cc9025597512
sophiaalexis1 <122618542+sophiaalexis1@users.noreply.github.com>
2023-05-18 22:01:53 -0400

    updated serializers to reflect updated POST command with foreignkey

--- cars/serializers.py
+++ cars/serializers.py
@@ -4,5 +4,6 @@ from .models import Car
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
-        fields = ['id', 'make', 'model', 'year', 'price', 'date_added', 'dealership']
+        fields = ['id', 'make', 'model', 'year', 'price', 'date_added', 'dealership', 'dealership_id']
         depth = 1
+    dealership_id = serializers.IntegerField(write_only=True)
